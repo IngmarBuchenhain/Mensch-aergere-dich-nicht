17813a3821808497f05b77505bdf0ed657932de1
Ingmar Buchenhain <71217928+IngmarBuchenhain@users.noreply.github.com>
2021-12-05 10:17:48 +0100

    Edit entry point

--- Source/CppMenschAergereDichNicht.cpp
+++ Source/CppMenschAergereDichNicht.cpp
@@ -1,9 +1,23 @@
-#include<iostream>
+#include "CppMenschAergereDichNicht.hpp"
+#include "CppDebugHelper.hpp"
 
 /**
  * Main entry point to the application.
+ * Creates needed objects and starts the logic.
  */
 int main(){
-    std::cout << "Hello World";
+    // Create In-Out-object
+
+    // Create Network-object
+
+    // Create logic-object with In-Out-object and Network-object as parameters
+
+    // Start logic-object
+    try{
+        printDebug("Now in try");
+    } catch(...){
+        printError("Failed to start game logic due to unknown Error!");
+        return -1;
+    }
     return 0;
 }
---
+++ Source/CppMenschAergereDichNicht.hpp
@@ -0,0 +1,10 @@
+#ifndef MENSCH_AERGERE_DICH_NICHT_HPP
+#define MENSCH_AERGERE_DICH_NICHT_HPP
+
+/**
+ * Main entry point to the application.
+ * Creates needed objects and starts the logic.
+ */
+int main();
+
+#endif
