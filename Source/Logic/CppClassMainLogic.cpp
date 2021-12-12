82d39d9adcf3afadbca8d7f69e1de8996dd062ff
IngmarBuchenhain <71217928+IngmarBuchenhain@users.noreply.github.com>
2021-12-12 19:51:08 +0100

    Update Develop-Logic  (#5)
    * Update README.md  and Add DebugHelper
    
    * Add DebugHelper
    
    * Move Figure class
    
    * Edit entry point
    
    * Update Makefile
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Update README.md
    
    * Update README.md
    
    Add a link to license  in main
    
    * Update README.md
    
    * Develop data (#4) Experiments
    
    * Add DebugHelper
    
    * Move Figure class
    
    * Edit entry point
    
    * Update Makefile
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Update README.md
    
    * Update README.md
    
    Add a link to license  in main
    
    * Update README.md
    
    * Update .gitignore
    
    * Play around and test stuff

---
+++ Source/Logic/CppClassMainLogic.cpp
@@ -0,0 +1,13 @@
+#include "CppClassMainLogic.hpp"
+#include "CppIUI.hpp"
+#include<iostream>
+
+  
+        MainLogic::MainLogic(int in){
+            test = in;
+            std::cout << test;
+        }
+
+        void MainLogic::start(){
+            
+        }
